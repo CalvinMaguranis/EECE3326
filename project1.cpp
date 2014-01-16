71fb80e25c52af28212a0bf725301015ccfa3135
Calvin <c.maguranis+github@gmail.com>
2014-01-16 11:34:40 -0500

    added more accessors and comments

--- project1.cpp
+++ project1.cpp
@@ -3,21 +3,35 @@
 
 using namespace std;
 
+/* MasterMind secret code class
+    generates a random code of size n with range m and compares user input
+    to random code
+*/
 class Code {
     public:
+        // constructors allow for either generating the random code or
+        // inputing a pre-defined code
         Code(const vector<int>&);
-        Code(const int, const int);
+        Code(const int n, const int m);
+
+        // accessors
+        vector<int> getCode() const { return code; }
+        void setCode(const vector<int> &v) { code = v; }
+        int getLength() const { return length; }
+        int getRange() const { return range; }
+
         int checkCorrect(const Code&) const;
         void print() const;
     private:
-        vector<int> getCode() const;
-        void setCode(const vector<int>&);
         vector<int> code;
+        const int length;
+        const int range;
 };
 
-Code::Code(const vector<int>& v) : code(v) {}
+/* Code class member function definitions */
+Code::Code(const vector<int>& v) : code(v), length(v.size()), range(0) {}
 
-Code::Code(const int n, const int m) {
+Code::Code(const int n, const int m) : length(n), range(m) {
     vector<int> v(n, 0);
     for (int i = 0; i < n; i++) {
         v[i] = rand() % m;
@@ -41,25 +55,20 @@ void Code::print() const {
     cout << endl;
 }
 
-vector<int> Code::getCode() const {
-    return code;
-}
+// prompt for secret code guess
+// note: separated from main for easier integration in the following projects
+Code makeGuess(const int len, const int range) {
+    cout << "Make a guess for the secret code ";
+    cout << "(code values range from 0 to " << range << "): ";
 
-void Code::setCode(const vector<int>& v) {
-    code = v;
-}
-
-Code makeGuess() {
-    int codeLength;
-    cout << "Enter code length: ";
-    cin >> codeLength;
-    vector<int> code(codeLength, 0);
-    for (int i = 0; i < codeLength; i++) {
+    // store guess one value at a time
+    vector<int> code(len, 0);
+    for (int i = 0; i < len; i++) {
         int value;
-        cout << "Enter value (0-9): ";
         cin >> value;
         code[i] = value;
     }
+
     Code guess(code);
     cout << "Your guess is: ";
     guess.print();
@@ -67,12 +76,24 @@ Code makeGuess() {
 }
 
 int main() {
+    int codeLength = 0;
+    int codeRange = 0;
+    int correct = 0;
+
+    cout << "Enter code length: ";
+    cin >> codeLength;
+    cout << "Enter code value range: ";
+    cin >> codeRange;
+    cout << endl << endl;
+
+    Code code(codeLength, codeRange);
     srand(time(NULL));
-    Code code(4, 10);
     cout << "Generated code: ";
     code.print();
-    Code guess = makeGuess();
-    int correct = code.checkCorrect(guess);
+
+    Code guess = makeGuess(code.getLength(), code.getRange());
+    correct = code.checkCorrect(guess);
     cout << "Numbers correct: " << correct << endl;
+
     return 1;
 }
