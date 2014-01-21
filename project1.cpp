fde9e0e21d6457faee1421608cbeb7a9095bc19b
Sam Coe <sam.coe@me.com>
2014-01-21 17:07:48 -0500

    Started part b of project. Still needs comments and error handling to be complete

--- project1.cpp
+++ project1.cpp
@@ -1,21 +1,22 @@
 /*****************************************************
-EECE3326 Optimization Methods
-Professor Waleed Meleis
+  EECE3326 Optimization Methods
+  Professor Waleed Meleis
 
-    Project #1a
-    Sam Coe
-    Calvin Maguranis
-*****************************************************/
+  Project #1b
+  Sam Coe
+  Calvin Maguranis
+ *****************************************************/
 
 #include <iostream>
 #include <vector>
 
 using namespace std;
 
-/* MasterMind secret code class
-    generates a random code of size n with range m and compares user input
-    to random code
-*/
+/*
+ * Mastermind secret code class
+ * generates a random code of size n with range m and compares user input
+ * to random code
+ */
 class Code {
     public:
         // constructors allow for either generating the random code or
@@ -26,17 +27,19 @@ class Code {
         // accessors
         vector<int> getCode() const { return code; }
         int getLength() const { return length; }
-        //int getRange() const { return range; }
 
         int checkCorrect(const Code&) const;
+        int checkIncorrect(const Code&) const;
         void print() const;
 
+        // overloaded output operator
+        friend ostream& operator<<(ostream&, const Code&);
+
     private:
+        void setCode(const vector<int> &v) { code = v; }
+
         vector<int> code;
         const int length;
-        //const int range;
-
-        void setCode(const vector<int> &v) { code = v; }
 };
 
 /* Code class member function definitions */
@@ -58,6 +61,23 @@ int Code::checkCorrect(const Code& c) const {
     return count;
 }
 
+int Code::checkIncorrect(const Code& c) const {
+    int count = 0;
+    vector<bool> used(getLength(), false);
+    for (int i = 0; i < c.getLength(); i++) {
+        for (int j = 0; j < getLength(); j++) {
+            if(!used[j] && c.getCode()[i] == getCode()[j]) {
+                used[j] = true;
+                if (i != j) {
+                    count++;
+                }
+                break;
+            }
+        }
+    }
+    return count;
+}
+
 void Code::print() const {
     vector<int> c = getCode();
     for (int i = 0; i < getLength(); i++) {
@@ -66,30 +86,85 @@ void Code::print() const {
     cout << endl;
 }
 
-// prompt for secret code guess
-// note: separated from main for easier integration in the following projects
-Code makeGuess(const int len, const int range) {
-    cout << "Make a guess for the secret code ";
-    cout << "(code values range from 0 to " << range-1 << "): ";
+// overload output operator for vectors
+template <typename T>
+ostream& operator<<(ostream& out, const vector<T>& v) {
+    for (int i = 0; i < v.size(); i++) {
+        out << v[i] << ' ';
+    }
+    return out;
+}
 
-    // store guess one value at a time
-    vector<int> code(len, 0);
-    for (int i = 0; i < len; i++) {
-        int value;
-        cin >> value;
-        code[i] = value;
+ostream& operator<<(ostream& out, const Code& c) {
+    out << c.code;
+    return out;
+}
+
+class Mastermind {
+    public:
+        Mastermind();
+        Mastermind(const int n, const int m);
+        void play() const;
+    private:
+        Code makeGuess() const;
+        const Code secret;
+        static const int defaultLength = 4;
+        static const int defaultRange = 10;
+        const int length;
+        const int range;
+};
+
+Mastermind::Mastermind() : secret(defaultLength, defaultRange),
+                           length(defaultLength),
+                           range(defaultRange){}
+
+    Mastermind::Mastermind(const int n, const int m) : secret(n, m),
+                                                       length(n),
+                                                       range(m){}
+
+    Code Mastermind::makeGuess() const {
+        cout << "Make a guess for the secret code ";
+        cout << "(code values range from 0 to " << range-1 << "): ";
+
+        // store guess one value at a time
+        vector<int> code(length, 0);
+        for (int i = 0; i < length; i++) {
+            int value;
+            cin >> value;
+            code[i] = value;
+        }
+
+        Code guess(code);
+        cout << "Your guess is: ";
+        cout << guess << endl;
+        return guess;
     }
 
-    Code guess(code);
-    cout << "Your guess is: ";
-    guess.print();
-    return guess;
+void Mastermind::play() const {
+    cout << "Secret code: " << secret << endl << endl;
+    int guesses = 10;
+
+    for (; guesses > 0; guesses--) {
+        cout << "Guesses left: " << guesses << endl;
+        Code guess = makeGuess();
+        int correct = secret.checkCorrect(guess);
+        if (correct == length) break;
+        int incorrect = secret.checkIncorrect(guess);
+        cout << "Numbers that are correct and in the correct position: " << correct << endl;
+        cout << "Numbers that are correct and not in the incorrect position: " << incorrect << endl << endl;
+    }
+
+    if (guesses > 0) {
+        cout << "You have won!" << endl;
+    } else {
+        cout << "You have lost!" << endl;
+        cout << "The secret code was: " << secret << endl;
+    }
 }
 
 int main() {
     int codeLength = 0;
     int codeRange = 0;
-    int correct = 0;
 
     srand(time(NULL));
 
@@ -97,15 +172,15 @@ int main() {
     cin >> codeLength;
     cout << "Enter code value range: ";
     cin >> codeRange;
-    cout << endl << endl;
+    cout << endl;
 
-    Code code(codeLength, codeRange);
-    cout << "Generated code: ";
-    code.print();
-
-    Code guess = makeGuess(code.getLength(), codeRange);
-    correct = code.checkCorrect(guess);
-    cout << "Numbers correct: " << correct << endl;
+    if (codeLength > 0 || codeRange > 0) {
+        Mastermind game;
+        game.play();
+    } else {
+        Mastermind game(codeLength, codeRange);
+        game.play();
+    }
 
     return 1;
 }
