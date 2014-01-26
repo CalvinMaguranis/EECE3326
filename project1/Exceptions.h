56ab6d86b4bab0ee8f55119f3e060f8fc5d09c5c
Calvin Maguranis <c.maguranis@gmail.com>
2014-01-26 17:50:42 -0500

    Missed updating Exceptions.h to std namespace

--- project1/Exceptions.h
+++ project1/Exceptions.h
@@ -11,23 +11,24 @@
 #define EXCEPTIONS_H
 #include <exception>
 #include <string>
+using namespace std;
 
-class BadInput : public std::exception {
-	// definition taken from stdexcept.h exception derived classes 
+class BadInput : public exception {
+	// definition taken from stdexcept.h exception derived classes
 public:
 	typedef exception exc;
 
-	explicit BadInput(const std::string& what) : exc(what.c_str()) {}
+	explicit BadInput(const string& what) : exc(what.c_str()) {}
 	explicit BadInput(const char* what) : exc(what) {}
 private:
 };
 
-class InvalidVectSize : public std::exception {
-	// definition taken from stdexcept.h exception derived classes 
+class InvalidVectSize : public exception {
+	// definition taken from stdexcept.h exception derived classes
 public:
 	typedef exception exc;
 
-	explicit InvalidVectSize(const std::string& what) : exc(what.c_str()) {}
+	explicit InvalidVectSize(const string& what) : exc(what.c_str()) {}
 	explicit InvalidVectSize(const char* what) : exc(what) {}
 private:
 };
