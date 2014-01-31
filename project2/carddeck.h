1d8b34e1a68a724cc682eb8aaf424a9bde9d87fd
Calvin Maguranis <c.maguranis@gmail.com>
2014-01-30 19:42:20 -0500

    Moved << operator overloads to main
    This was the problem I ran into on project 1, too. We should keep all
    global overloads in main.cpp from now on.

--- project2/carddeck.h
+++ project2/carddeck.h
@@ -31,44 +31,6 @@ class deck {
         node<card> *topCard;
 };
 
-// global print overload for card class
-ostream& operator<<(ostream &ost, const card &c) {
-    string suit;
-    string face_card; // when we have Ace, King, Queen or, Jack
 
-    // determine our suit
-    if (c.getSuit() == CLUB) { suit = "Clubs"; }
-    else if (c.getSuit() == DIAMOND) { suit = "Diamonds"; }
-    else if (c.getSuit() == HEART) { suit = "Hearts"; }
-    else if (c.getSuit() == SPADE) { suit = "Spades"; }
-    else { throw rangeError("Invalid suit type"); }
-
-    // determine if we have a face card
-    if (c.getValue() >= 2 && c.getValue() <= 10) {
-        // we have a normal value, just print it out and return
-        ost << c.getValue() << " of " << suit;
-    }
-    else { // face card
-        if (c.getValue() == JOKER) { face_card = "Joker"; }
-        else if (c.getValue() == ACE) { face_card = "Ace"; }
-        else if (c.getValue() == KING) { face_card = "King"; }
-        else if (c.getValue() == QUEEN) { face_card = "Queen"; }
-        else if (c.getValue() == JACK) { face_card = "Jack"; }
-        else { throw rangeError("Card value out of range!"); } // catch an invalid card value
-        ost << face_card << " of " << suit;
-    }
-
-    return ost;
-}
-
-// global print overload for deck class
-ostream& operator<<(ostream &ost, const deck &d) {
-    node<card>* curr = d.getTopCard();
-    while (curr != NULL) {
-        ost << curr->nodeValue;
-        curr = curr->next;
-    }
-    return ost;
-}
 
 #endif /* CARDDECK_H */
--- project2/project2.cpp
+++ project2/project2.cpp
@@ -1,6 +1,47 @@
+#include <iostream>
 #include "carddeck.h"
 
 using namespace std;
+
+// global print overload for card class
+ostream& operator<<(ostream &ost, const card &c) {
+	string suit;
+	string face_card; // when we have Ace, King, Queen or, Jack
+
+	// determine our suit
+	if (c.getSuit() == CLUB) { suit = "Clubs"; }
+	else if (c.getSuit() == DIAMOND) { suit = "Diamonds"; }
+	else if (c.getSuit() == HEART) { suit = "Hearts"; }
+	else if (c.getSuit() == SPADE) { suit = "Spades"; }
+	else { throw rangeError("Invalid suit type"); }
+
+	// determine if we have a face card
+	if (c.getValue() >= 2 && c.getValue() <= 10) {
+		// we have a normal value, just print it out and return
+		ost << c.getValue() << " of " << suit;
+	}
+	else { // face card
+		if (c.getValue() == JOKER) { face_card = "Joker"; }
+		else if (c.getValue() == ACE) { face_card = "Ace"; }
+		else if (c.getValue() == KING) { face_card = "King"; }
+		else if (c.getValue() == QUEEN) { face_card = "Queen"; }
+		else if (c.getValue() == JACK) { face_card = "Jack"; }
+		else { throw rangeError("Card value out of range!"); } // catch an invalid card value
+		ost << face_card << " of " << suit;
+	}
+
+	return ost;
+}
+
+// global print overload for deck class
+ostream& operator<<(ostream &ost, const deck &d) {
+	node<card>* curr = d.getTopCard();
+	while (curr != NULL) {
+		ost << curr->nodeValue << endl;
+		curr = curr->next;
+	}
+	return ost;
+}
 
 int main() {
     try {
@@ -14,5 +55,6 @@ int main() {
         exit(0);
     }
 
+	system("PAUSE");
     return 1;
 }
