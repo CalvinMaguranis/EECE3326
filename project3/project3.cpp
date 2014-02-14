5b9b1584a74f25a76bf3a30ad461a9a736ef47ba
CalvinMaguranis <c.maguranis@gmail.com>
2014-02-13 19:21:59 -0500

    updated main file

--- project3/project3.cpp
+++ project3/project3.cpp
@@ -6,83 +6,68 @@
 
 using namespace std;
 
-class Grid {
-    public:
-        Grid();
-        matrix<char> getMatrix();
-        void setMatrix(matrix<char>);
-        void setMatrix(string);
-    private:
-        matrix<char> m;
-};
+// to preserve the direction we compare our strings in
+enum DIRECTION { NONE=0, UP, DOWN, LEFT, RIGHT, UP_LEFT, UP_RIGHT, DOWN_LEFT, DOWN_RIGHT }
 
-Grid::Grid() : m(matrix<char>()) {}
-
-matrix<char> Grid::getMatrix() {
-    return m;
-}
-
-void Grid::setMatrix(matrix<char> nm) {
-    m = nm;
-}
-
-void Grid::setMatrix(string s) {
-    ifstream f(s, ios::in);
-    if (f.is_open()) {
-        int rows, cols;
-        f >> rows >> cols;
-        m.resize(rows, cols);
-        for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < cols; j++) {
-                f >> m[i][j];
+void findMatches(wordlist words, Grid g) {
+    vector<string> list = words.getList();
+    bool found = false;
+    for(vector<string>::iterator it = list.begin(); it != list.end(); ++it) {
+        // access every word in the list
+        for (int row=0; row<g.getMatrix.rows(); row++) {
+            // check every row in the matrix
+            for (int col=0; col<g.getMatrix.cols(); col++) {
+                // check every row in the matrix
+                found = findWord(*it, 0, g, row, col, NONE);
+                if (found) { 
+                    cout << *it << endl; 
+                    break; // end search
+                }
             }
+            if (found) { break; }
         }
-        f.close();
+        if (found) { break; } // go to next word
+    }
+}
+
+bool findWord(Grid g, string word, int pos, int row, int col, DIRECTION d) {
+    // check base cases
+    if (word[pos] != g.getMatrix()[row][col]) { 
+        // mismatch found, continue to next row, col
+        return false; 
+    }
+    if ((word.size() == pos) && (word[pos] != g.getMatrix()[row][col])) { 
+        // if we are at the last word
+        return true; 
+    }
+
+    if (d == NONE) { 
+        return neighbors(g, word, &(pos+1), row, col, d,);
+
     } else {
-        cout << "Unable to open file" << endl;
+        switch
+            case direction
+                UP
+                    col-- % g.cols()
+                down
+                    col++ % g.cols()
+                left
+                    rows
+                right
+                    rows
+
+        findWord(word, pos+1, g, row, col, direction);
     }
 }
 
-class wordlist {
-    public:
-        wordlist(); // do nothing
-        wordlist(string file);
-
-        void open(string file);
-        string getWord();
-
-        vector<string> words;
-};
-
-wordlist::wordlist() {
-}
-
-wordlist::wordlist(string file) {
-    open(file);
-}
-
-void wordlist::open(string file) {
-    fstream ifs;
-    string word;
-
-    ifs.open(file.c_str());
-    if (ifs.fail()) { throw fileOpenError("failed to open file"); }
-
-    while (getline(ifs, word)) {
-        words.push_back(word);
+bool neighbors(Grid g, string word, int pos, int row, int col, DIRECTION d) {
+    const int max_neighbors = 8; // any char will have 8 neighbors
+    for (int i=0; i<max_neighbors; i++) {
+        // check ALL neighbors for the next char, 
+        // there could be multiple chars of the same value
     }
-}
 
-string wordlist::getWord() {
-    string word;
-    if (words.empty()) {
-        return "1";
-    }
-    word = words.back(); // get last word
-    words.pop_back(); // remove from vector
-    return word;
 }
-
 /*
 int main() {
     try {
@@ -117,36 +102,4 @@ int main() {
     }
 }
 
-void findMatches(wordlist words, Grid g) {
-    for(std::vector<string>::iterator it = words.words.begin(); it != words.words.end(); ++it) {
-        bool found = findWord(*it, 0, g, 0, 0, NULL);
-        if (found) cout << *it;
-    }
-}
 
-bool findWord(string word, int pos, Grid g, int row, int col, DIRECTION) {
-    CHECK if word[pos] != g[row][col] return false
-
-    if at end of word return true
-
-    if (DIRECTION == ANY) {
-        CHECK AJACENT CHARACTERS
-        if (any match ajacent) {
-            findWord(word, pos+1, g, row, col, direction);
-        }
-
-    } else {
-        switch
-            case direction
-                UP
-                    col-- % g.cols()
-                down
-                    col++ % g.cols()
-                left
-                    rows
-                right
-                    rows
-
-        findWord(word, pos+1, g, row, col, direction);
-    }
-}
